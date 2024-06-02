117117ca3a124e3d03dd671b8d5600d21dfb927e
Sk3letr0n <griloven@gmail.com>
2024-06-02 00:09:47 -0500

    Paginas Web

---
+++ Prácticas JS/Practica 1/js/script.js
@@ -0,0 +1,21 @@
+
+function mostrar(valor) {
+    const resultado = document.getElementById('resultado').value;
+    document.getElementById('resultado').value = valor + resultado;
+}
+
+function suma(num1, num2) {
+    return num1 + num2;
+}
+
+function resta(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplicacion(num1, num2) {
+    return num1 * num2;
+}
+
+function division(num1, num2) {
+    return num1 / num2;
+}
